ae58850b0c289e10935d8df819240e47016e4f22
Vitor Hugo M. Beck <toorugoo@gmail.com>
2015-10-08 10:27:49 -0300

    Permission for android M Still trying to read SIM 2 IMSI

--- app/src/main/java/com/vitorhugobeck/simtest/MainActivity.java
+++ app/src/main/java/com/vitorhugobeck/simtest/MainActivity.java
@@ -1,8 +1,12 @@
 package com.vitorhugobeck.simtest;
 
+import android.Manifest;
 import android.annotation.TargetApi;
 import android.content.Context;
+import android.content.pm.PackageManager;
 import android.os.Build;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.telephony.SubscriptionInfo;
@@ -25,77 +29,122 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        if (android.os.Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+
+            if (ContextCompat.checkSelfPermission(this,
+                    Manifest.permission.READ_PHONE_STATE)
+                    != PackageManager.PERMISSION_GRANTED) {
+
+                // Should we show an explanation?
+                if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                        Manifest.permission.READ_PHONE_STATE)) {
+
+                    Toast.makeText(getApplicationContext(), "give it", Toast.LENGTH_LONG).show();
+
+                } else {
+
+                    // No explanation needed, we can request the permission.
+
+                    ActivityCompat.requestPermissions(this,
+                            new String[]{Manifest.permission.READ_PHONE_STATE},
+                            1);
+
+                    // MY_PERMISSIONS_REQUEST_READ_CONTACTS is an
+                    // app-defined int constant. The callback method gets the
+                    // result of the request.
+                }
+            }
+            else {
+                super.onDestroy();
+            }
+
+        }
+
         final Button btnData = (Button) findViewById(R.id.btnGet);
         btnData.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 getSimData();
             }
-    });
+        });
     }
-    @TargetApi(Build.VERSION_CODES.LOLLIPOP_MR1)
-    void getSimData(){
-        SubscriptionManager manager = SubscriptionManager.from(this);
-        List<SubscriptionInfo> sil = manager.getActiveSubscriptionInfoList();
 
-        if (sil != null) {
-            for (SubscriptionInfo subInfo : sil) {
-                Log.v("TestVitor", "SubInfo:" + subInfo.getSubscriptionId());
+    void getSimData() {
+
+        if (android.os.Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP_MR1) {
+            if (android.os.Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_CONTACTS) != PackageManager.PERMISSION_GRANTED) {
+                    ActivityCompat.requestPermissions(this,
+                            new String[]{Manifest.permission.READ_CONTACTS},
+                            0);
+                }
+            }
+
+            SubscriptionManager manager = SubscriptionManager.from(this);
+            List<SubscriptionInfo> sil = manager.getActiveSubscriptionInfoList();
+
+            if (sil != null) {
+                for (SubscriptionInfo subInfo : sil) {
+                    Log.v("SIMTest Log", "SIM: " + String.valueOf(subInfo.getSubscriptionId()) + " MCC: " + String.valueOf(subInfo.getMcc()));
+                }
+            } else {
+                Log.v("TestMain", "SubInfo: list is null");
             }
         } else {
-            Log.v("TestMain", "SubInfo: list is null");
+            String dbgMsg = "";
+            TelephonyManager telephonyManager = (TelephonyManager) getSystemService(Context.TELEPHONY_SERVICE);
+            int phoneCount = 0;
+            int simState = telephonyManager.getSimState();
+            String simOperatorCode = null; //, simCountry, simOperatorName, simSerial;
+            switch (simState) {
+
+                case (TelephonyManager.SIM_STATE_ABSENT): {
+                    Toast.makeText(getApplicationContext(), "NO SIM PRESENT", Toast.LENGTH_LONG).show();
+                    break;
+                }
+                case (TelephonyManager.SIM_STATE_NETWORK_LOCKED):
+                    break;
+                case (TelephonyManager.SIM_STATE_PIN_REQUIRED):
+                    break;
+                case (TelephonyManager.SIM_STATE_PUK_REQUIRED):
+                    break;
+                case (TelephonyManager.SIM_STATE_UNKNOWN):
+                    break;
+                case (TelephonyManager.SIM_STATE_READY): {
+
+                    // Get the SIM country ISO code
+                    ((TextView) findViewById(R.id.isoCode)).setText(telephonyManager.getSimCountryIso());
+
+                    // Get the operator code of the active SIM (MCC + MNC)
+                    ((TextView) findViewById(R.id.simMCC)).setText(telephonyManager.getSimOperator());
+
+                    // Get the name of the SIM operator
+                    ((TextView) findViewById(R.id.operatorName)).setText(telephonyManager.getSimOperatorName());
+
+                    // Get the SIM’s serial number
+                    ((TextView) findViewById(R.id.SerialNumber)).setText(telephonyManager.getSimSerialNumber());
+
+                    //Get the SIM's IMSI
+                    ((TextView) findViewById(R.id.IMSICode)).setText(telephonyManager.getSubscriberId());
+
+                    //Print the time to assure the data is recently collected
+
+                    DateFormat df = new SimpleDateFormat("HH:mm:ss");
+                    Calendar cal_hoje = Calendar.getInstance();
+                    ((TextView) findViewById(R.id.time)).setText(df.format(cal_hoje.getTime()));
+                    Toast.makeText(getApplicationContext(), "Data Collected", Toast.LENGTH_LONG).show();
+
+                    int currentapiVersion = android.os.Build.VERSION.SDK_INT;
+                    ((TextView) findViewById(R.id.debugMessages)).setText(dbgMsg);
+                    break;
+                }
+            }
         }
+
     }
-//    void getSimData() {
-//        String dbgMsg = "";
-//        TelephonyManager  telephonyManager = (TelephonyManager)getSystemService(Context.TELEPHONY_SERVICE);
-//        int phoneCount=0;
-//        int simState = telephonyManager.getSimState();
-//        String simOperatorCode = null; //, simCountry, simOperatorName, simSerial;
-//        switch (simState) {
-//
-//            case (TelephonyManager.SIM_STATE_ABSENT): {
-//                Toast.makeText(getApplicationContext(), "NO SIM PRESENT", Toast.LENGTH_LONG).show();
-//                break;
-//            }
-//            case (TelephonyManager.SIM_STATE_NETWORK_LOCKED): break;
-//            case (TelephonyManager.SIM_STATE_PIN_REQUIRED): break;
-//            case (TelephonyManager.SIM_STATE_PUK_REQUIRED): break;
-//            case (TelephonyManager.SIM_STATE_UNKNOWN): break;
-//            case (TelephonyManager.SIM_STATE_READY): {
-//
-//                // Get the SIM country ISO code
-//                ((TextView)findViewById(R.id.isoCode)).setText(telephonyManager.getSimCountryIso());
-//
-//                // Get the operator code of the active SIM (MCC + MNC)
-//                ((TextView)findViewById(R.id.simMCC)).setText(telephonyManager.getSimOperator());
-//
-//                // Get the name of the SIM operator
-//                ((TextView)findViewById(R.id.operatorName)).setText(telephonyManager.getSimOperatorName());
-//
-//                // Get the SIM’s serial number
-//                ((TextView)findViewById(R.id.SerialNumber)).setText(telephonyManager.getSimSerialNumber());
-//
-//                //Get the SIM's IMSI
-//                ((TextView)findViewById(R.id.IMSICode)).setText(telephonyManager.getSubscriberId());
-//
-//                //Print the time to assure the data is recently collected
-//
-//                DateFormat df = new SimpleDateFormat("HH:mm:ss");
-//                Calendar cal_hoje = Calendar.getInstance();
-//                ((TextView)findViewById(R.id.time)).setText(df.format(cal_hoje.getTime()));
-//                Toast.makeText(getApplicationContext(), "Data Collected", Toast.LENGTH_LONG).show();
-//
-//                int currentapiVersion = android.os.Build.VERSION.SDK_INT;
-//                if (currentapiVersion >= Build.VERSION_CODES.LOLLIPOP_MR1){
-//                    dbgMsg = String.valueOf(telephonyManager.getPhoneCount());
-//                }
-//                ((TextView)findViewById(R.id.debugMessages)).setText(dbgMsg);
-//                break;
-//            }
-//        }
-//    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
